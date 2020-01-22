967c37b71a4d8513f39ccf15b6f207641ee397c7
Madhuparna Bhowmik <madhuparnabhowmik10@gmail.com>
2020-01-22 11:02:43 +0530

    final

--- Img_Preprocessing/find_harvest.py
+++ Img_Preprocessing/find_harvest.py
@@ -10,8 +10,6 @@ def find_harvest():
 	dim_y = imgs.shape[2]
 	print(num_images, dim_x, dim_y)
 	diff = np.zeros((num_images-1,dim_x,dim_y))
-	plt.imshow(imgs[7], cmap="gray")
-	plt.show()
 	j=0
 	for i in range(0, num_images-1 ,1):
 	    diff[j] = (( imgs[i+1]) - (imgs[i]))
@@ -32,6 +30,35 @@ def find_harvest():
 	print(x,y)
 	plt.plot(x,y)
 	plt.savefig('plot.png')
+
+
+	months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+	harvest_threshold = 2000
+	number_of_harvests = 0
+	maturity_month = []
+	harvested_month = []
+	crop_cycle = 0
+	for i in range(int(len(x)/2)-1):
+		print(y[i+1]-y[i])
+		if(y[i+1]-y[i] > harvest_threshold):
+			number_of_harvests+=1
+			maturity_month.append(months[i])
+			harvested_month.append(months[i+1])
+
+	crop_cycle = 12/number_of_harvests
+	
+	data = {}
+	data['number_of_harvests'] = number_of_harvests
+	data['maturity_month'] = maturity_month
+	data['harvested_month'] = harvested_month
+	data['crop_cycle'] = crop_cycle
+	data['crop_type'] = "crop X"
+	print(data)
+	return data
+	
+			
+
+			
 
 find_harvest()
             
--- Img_Preprocessing/preprocessing.py
+++ Img_Preprocessing/preprocessing.py
@@ -8,46 +8,51 @@ import numpy as np
 import read_images
 from PIL import Image
 
-images = read_images.load_images_from_folder("../Dataset")
+#images = read_images.load_images_from_folder("../Dataset")
 contrast = 10
 blur_kernel_size = 10
 noise_kernel_size = 10
 resize_factor = 0.60
 
 def load_images():
-	images=[]
+	ar = np.zeros((24,2118,2135))
 	for i in range(1,13,1):
 		if i <=9:
 		    st = "0"+ str(i)
 		else:
 		    st = str(i)
-		img = cv2.imread('../Dataset/awifs_ndvi_2017'+st+'_15_1_clipped.tif')
-		images.append(img)
+		img = Image.open('../Dataset/awifs_ndvi_2017'+st+'_15_1_clipped.tif')
+		img.seek(0)
+		for x in range(0,2118):
+		    for y in range(0,2135):
+		        ar[i-1][x][y] = img.getpixel((x, y))
+	for i in range(1,13,1):
+		if i <=9:
+		    st = "0"+ str(i)
+		else:
+		    st = str(i)
+		img = Image.open('../Dataset/awifs_ndvi_2018'+st+'_15_1_clipped.tif')
+		img.seek(0)
+		for x in range(0,2118):
+		    for y in range(0,2135):
+		        ar[i-1 + 12][x][y] = img.getpixel((x, y))
+	np.save("save_img_arr",ar)
+	return ar
 
-	
-	for i in range(1,13,1):
-		if i <=9:
-		    st = "0"+ str(i)
-		else:
-		    st = str(i)
-		img = cv2.imread('../Dataset/awifs_ndvi_2018'+st+'_15_1_clipped.tif')
-		images.append(img)
-	return images
 
 def preprocessing():
-    images = load_images()
+    images = np.load("save_img_arr.npy")
     preprocessed = []
+
     for i in range(len(images)):
-        temp = images[i]#[:, :, 0]
-        '''temp = noise.denoise(images[i], noise_kernel_size)
+        temp = images[i]
         temp = blur.blur(temp, blur_kernel_size)
         temp = normalize.normalize(temp)
         temp = change_contrast.change_contrast(temp, contrast)
-        height, width, channels = temp.shape
-        temp = resize.resize(temp,int(height*resize_factor), int(width*resize_factor))'''
-        #cv2.imshow("Preprocessed", temp)	
-        preprocessed.append(temp[:, :, 0])
-    preprocessed_array = np.array(preprocessed)
+        height, width = temp.shape
+        temp = resize.resize(temp,int(height*resize_factor), int(width*resize_factor))	
+        preprocessed.append(temp)
+    preprocessed_array = np.array(preprocessed,dtype=np.int64)
     print("Preprocessing done!", len(preprocessed))
     return preprocessed_array
 
